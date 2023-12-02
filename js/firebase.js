4ecc330635bd332df17ac6b73eb8d1c5e6258606
Mhadathy99 <mateohadathy@yahoo.es>
2023-12-01 20:57:26 -0800

    Initialazing Firebase

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+/*FIREBASE CONFIGURATIONS */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBs76g9PUueXOXKp1Wfir_hcfvEjSg8eWI",
+    authDomain: "assesment-3-cctb.firebaseapp.com",
+    projectId: "assesment-3-cctb",
+    storageBucket: "assesment-3-cctb.appspot.com",
+    messagingSenderId: "1061488594598",
+    appId: "1:1061488594598:web:577e8399c48e45fd6aae67"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
